5eb1cc43a79ce95690b9f9f585ac69218f7ed05b
sarangdaddy <109648042+sarangdaddy@users.noreply.github.com>
2022-08-30 17:54:29 +0900

    login

---
+++ js/login.js
@@ -0,0 +1,34 @@
+const loginForm = document.querySelector(".login-form");
+const loginInput = loginForm.querySelector("input");
+const home = document.querySelector(".home");
+const welcomeUserName = document.querySelector(".header_welcome_username");
+const logoutButton = home.querySelector(".logout_button");
+
+const HIDDEN_KEY = "hidden";
+const USERNAME_KEY = "username";
+
+function handleLoginSubmit(event) {
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_KEY);
+  const inputValue = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, inputValue);
+  welcomeUserName.innerText = `Welcome ${inputValue}`;
+  home.classList.remove(HIDDEN_KEY);
+}
+
+function handelLogoutClick() {
+  loginForm.classList.remove(HIDDEN_KEY);
+  home.classList.add(HIDDEN_KEY);
+}
+
+logoutButton.addEventListener("click", handelLogoutClick);
+
+const localStorageUsername = localStorage.getItem(USERNAME_KEY);
+
+if (localStorageUsername === null) {
+  loginForm.classList.remove(HIDDEN_KEY);
+  loginForm.addEventListener("submit", handleLoginSubmit);
+} else {
+  welcomeUserName.innerText = `Welcome ${localStorageUsername}`;
+  home.classList.remove(HIDDEN_KEY);
+}
