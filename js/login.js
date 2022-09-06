62915da2665c4203ca856d35a3099a0275f24b33
Sung Un KIM <ksu8904@gmail.com>
2022-09-07 00:01:37 +0900

    doing css

--- js/login.js
+++ js/login.js
@@ -3,12 +3,15 @@ const loginInput = loginForm.querySelector("input");
 const home = document.querySelector(".home");
 const welcomeUserName = document.querySelector(".header_welcome_username");
 const logoutButton = home.querySelector(".logout_button");
+const body = document.body;
 
 const HIDDEN_KEY = "hidden";
 const USERNAME_KEY = "username";
 
 function handleLoginSubmit(event) {
   event.preventDefault();
+  body.classList.add("home__body");
+  body.classList.remove("login__body");
   loginForm.classList.add(HIDDEN_KEY);
   const inputValue = loginInput.value;
   localStorage.setItem(USERNAME_KEY, inputValue);
@@ -21,6 +24,8 @@ function paintUserName(username) {
 }
 
 function handelLogoutClick() {
+  body.classList.remove("home__body");
+  body.classList.add("login__body");
   loginForm.classList.remove(HIDDEN_KEY);
   home.classList.add(HIDDEN_KEY);
   localStorage.removeItem(USERNAME_KEY);
@@ -32,6 +37,8 @@ const localStorageUsername = localStorage.getItem(USERNAME_KEY);
 
 if (localStorageUsername === null) {
   loginForm.classList.remove(HIDDEN_KEY);
+  body.classList.remove("home__body");
+  body.classList.add("login__body");
   loginForm.addEventListener("submit", handleLoginSubmit);
 } else {
   paintUserName(localStorageUsername);
